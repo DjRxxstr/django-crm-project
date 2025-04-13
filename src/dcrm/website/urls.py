d6de8e63f0ca2e8c806dcbae84d4c1c38b4a0af1
DjRxxstr <dhyanj2016@gmail.com>
2025-04-13 19:20:16 +0530

    Updated views.py with login logic, minor changes in other files

--- src/dcrm/website/urls.py
+++ src/dcrm/website/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from .views import (
     home_page_view,
-    login_user,
-    logout_user);
+    # login_user,
+    # logout_user
+    );
 
 urlpatterns = [
     path('', home_page_view, name = 'home-page-view'),
-    path('login/', login_user, name = 'login-user'),
-    path('logout/', logout_user, name = 'logout-user'),
+    # path('login/', login_user, name = 'login-user'),
+    # path('logout/', logout_user, name = 'logout-user'),
 ]
--- src/dcrm/website/views.py
+++ src/dcrm/website/views.py
@@ -1,12 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 
 def home_page_view(request):
-    return render(request, 'home.html', {})
+    #To check if logging in
+    #If logging in -> request will be POST
+    #If already logged in -> request will be GET
 
-def login_user(request):
-    pass
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You Have Been Logged In!")
+            return redirect('home-page-view')
+        else:
+            messages.success(request, "There was an error inlogging in..." )
+            return redirect('home-page-view')
+
+    else:
+        return render(request, 'home.html', {})
+
+# def login_user(request):
+#     pass
 
 def logout_user(request):
     pass
