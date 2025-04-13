d3ebc04a2e76e9ed42da8ae17300350d971b4bf5
DjRxxstr <dhyanj2016@gmail.com>
2025-04-13 12:49:46 +0530

    Added login and logout to views.py, updated urls.py accordingly

--- src/dcrm/website/urls.py
+++ src/dcrm/website/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import home_page_view;
+from .views import (
+    home_page_view,
+    login_user,
+    logout_user);
 
 urlpatterns = [
     path('', home_page_view, name = 'home-page-view'),
+    path('login/', login_user, name = 'login-user'),
+    path('logout/', logout_user, name = 'logout-user'),
 ]
--- src/dcrm/website/views.py
+++ src/dcrm/website/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home_page_view(request):
     return render(request, 'home.html', {})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    pass
