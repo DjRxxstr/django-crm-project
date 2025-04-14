814095faa4439c4129af365fc3e2713d3fa6d4a2
DjRxxstr <dhyanj2016@gmail.com>
2025-04-15 03:07:57 +0530

    Now displaying records with bootstrap table

--- src/dcrm/website/models.py
+++ src/dcrm/website/models.py
@@ -5,6 +5,8 @@ class Record(models.Model):
     created_at  = models.DateTimeField(auto_now_add = True)
     first_name  = models.CharField(max_length = 50)
     last_name   = models.CharField(max_length = 50)
+    email       = models.CharField(max_length = 50, null = True, blank = True)
+    phone       = models.CharField(max_length = 15, null = True, blank  = True)
     address     = models.CharField(max_length = 100)
     city        = models.CharField(max_length = 50)
     state       = models.CharField(max_length = 50)
