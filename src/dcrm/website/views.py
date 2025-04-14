5b04fc75649e266f5fdba1af3389a90619559c0f
DjRxxstr <dhyanj2016@gmail.com>
2025-04-15 00:18:15 +0530

    Updated registration logic

--- src/dcrm/website/views.py
+++ src/dcrm/website/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
 
 def home_page_view(request):
     #To check if logging in
@@ -33,4 +34,42 @@ def logout_user(request):
     return redirect('home-page-view')
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    form = SignUpForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password1']
+        user = authenticate(username=username, password=password)
+        login(request, user)
+        messages.success(request, "You have Successfully Registered!")
+        return redirect('home-page-view')
+
+    context = {
+        'form' : form
+    }
+
+    return render(request, 'register.html', context)
+
+    # if request.method == 'POST':
+    #     form = SignUpForm(request.POST or None)
+
+    #     if form.is_valid():
+    #         form.save()
+
+    #         username = form.cleaned_data['username']
+    #         password = form.cleaned_data['password']
+    #         user = authenticate(username=username, password=password)
+    #         login(request, user)
+    #         messages.success(request, "You have Successfully Registered!")
+    #         return redirect('home-page-view')
+
+    # else:
+    #     form = SignUpForm() #Default request is 'GET'
+    #     context = {
+    #         'form' : form
+    #     }
+    #     return render(request, 'register.html', context)
+        
+    # return render(request, 'register.html', context)
