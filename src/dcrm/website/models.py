47c5aef8d50b459c2af4e8aab64f49cca10a6dd6
DjRxxstr <dhyanj2016@gmail.com>
2025-04-15 00:33:59 +0530

    Added Record model in models.py, also migrated the changes

--- src/dcrm/website/models.py
+++ src/dcrm/website/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+class Record(models.Model):
+    created_at  = models.DateTimeField(auto_now_add = True)
+    first_name  = models.CharField(max_length = 50)
+    last_name   = models.CharField(max_length = 50)
+    address     = models.CharField(max_length = 100)
+    city        = models.CharField(max_length = 50)
+    state       = models.CharField(max_length = 50)
+    zipcode     = models.CharField(max_length = 10)
+
+    # We use __str__ to define how the object is displayed
+    # __init__ is for object creation
+    # __str__ makes it easier to identify objects when printing or viewing them.
+
+
+    def __str__(self):
+        # just to show full name when printing the object (like in admin)
+        return (f"{self.first_name} {self.last_name}")
